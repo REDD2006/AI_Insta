d86a0036e83689e2f58af824e89b12ac4c8f545d
REDD2006 <rredd797@gmail.com>
2023-07-19 09:24:27 +0530

    Initial commit

---
+++ app/src/main/java/com/adetrez/ai_insta/ForgetPasswordActivity.java
@@ -0,0 +1,14 @@
+package com.adetrez.ai_insta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForgetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_password);
+    }
+}
